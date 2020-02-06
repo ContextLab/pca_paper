3e0253a46f2cea7af706e6f8254e6379287eaaad
lucy <lucywowen@gmail.com>
2020-02-06 14:28:17 -0500

    redo analyses with 100 dims

--- code/scripts/config.py
+++ code/scripts/config.py
@@ -7,13 +7,13 @@ config['template'] = 'run_job.sh'
 
 # ====== MODIFY ONLY THE CODE BETWEEN THESE LINES ======
 if (socket.gethostname() == 'Lucys-MacBook-Pro-3.local') or (socket.gethostname() == 'vertex.kiewit.dartmouth.edu') or (socket.gethostname() == 'vertex.local')or (socket.gethostname() == 'vpn-two-factor-general-230-139-212.dartmouth.edu'):
-    config['datadir'] = '/Users/lucyowen/Desktop/timecorr_env/timecorr_paper/pieman/data'
-    config['workingdir'] = '/Users/lucyowen/Desktop/timecorr_env/timecorr_paper/pieman'
+    config['datadir'] = '/Users/lucyowen/Desktop/pca_env/pca_paper/pieman/data'
+    config['workingdir'] = '/Users/lucyowen/Desktop/pca_env/pca_paper/pieman'
     config['startdir'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # directory to start the job in
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job_local.sh')
 else:
-    config['datadir'] = '/dartfs/rc/lab/D/DBIC/CDL/f002s72/timecorr_paper/pieman/data'
-    config['workingdir'] = '/dartfs/rc/lab/D/DBIC/CDL/f002s72/timecorr_paper/pieman'
+    config['datadir'] = '/dartfs/rc/lab/D/DBIC/CDL/f002s72/pca_paper/pieman/data'
+    config['workingdir'] = '/dartfs/rc/lab/D/DBIC/CDL/f002s72/pca_paper/pieman'
     config['startdir'] = '/dartfs/rc/lab/D/DBIC/CDL/f002s72'
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job.sh')
 
@@ -23,7 +23,7 @@ config['lockdir'] = os.path.join(config['workingdir'], 'locks')
 config['resultsdir'] = os.path.join(config['workingdir'], 'results')
 
 # runtime options
-config['jobname'] = "timecorr_analysis"  # default job name
+config['jobname'] = "pca_analysis"  # default job name
 config['q'] = "default"  # options: default, testing, largeq
 config['nnodes'] = 1  # how many nodes to use for this one job
 config['ppn'] = 4  # how many processors to use for this one job (assume 4GB of RAM per processor)
