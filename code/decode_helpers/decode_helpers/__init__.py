8bd7a53612c1507c300fd22eab42b0646fdef1b6
jeremymanning <jeremy.r.manning@dartmouth.edu>
2023-03-12 12:34:47 -0400

    got rid of unused code

--- code/decode_helpers/decode_helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-version_info = (0, 0, 1)
-__version__ = '.'.join(map(str, version_info))
--- code/decode_helpers/setup.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup
-
-
-# reads from setup.cfg
-setup()
